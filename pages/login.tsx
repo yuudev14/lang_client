43675b33f3e797706f993a3c33c29b041e264f47
yutakaki <takaki.yu.dev@gmail.com>
2022-08-12 18:41:59 +0900

    initial commit

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <div>
+      <form>
+        <h1 className="">Login</h1>
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
+
+export default login;

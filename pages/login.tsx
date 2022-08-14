353fe570fd45127460b88c07f44d01da543d95cf
yutakaki <takaki.yu.dev@gmail.com>
2022-08-14 17:54:18 +0900

    login page finish

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,20 +1,63 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-const login = () => {
+type loginFormStateType = {
+  usernameOrEmail: string;
+  password: string;
+};
+
+const yupSchema = yup
+  .object({
+    usernameOrEmail: yup.string().required(),
+    password: yup.string().required(),
+  })
+  .required();
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<loginFormStateType>({
+    resolver: yupResolver(yupSchema),
+  });
+
+  const login = (values: loginFormStateType) => {
+    console.log(values);
+  };
   return (
     <main>
-      <form className="form">
+      <form
+        className="form hover:border-gray-300"
+        onSubmit={handleSubmit(login)}>
         <h1 className="text-3xl font-bold text-center mb-10">Login</h1>
         <input
           type="text"
           className="input-text"
           placeholder="Username or Email"
+          {...register("usernameOrEmail")}
         />
-        <input type="password" className="input-text" placeholder="Password" />
+
+        {errors.usernameOrEmail && (
+          <p className="ml-2 text-red-500">{errors.usernameOrEmail.message}</p>
+        )}
+
+        <input
+          type="password"
+          className="input-text"
+          placeholder="Password"
+          {...register("password")}
+        />
+
+        {errors.password && (
+          <p className="ml-2 text-red-500">{errors.password.message}</p>
+        )}
         <button className="btn mt-3">Login</button>
       </form>
     </main>
   );
 };
 
-export default login;
+export default Login;

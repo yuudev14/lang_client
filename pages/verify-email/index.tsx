2b1eecdc47f23f4a8959dc1b0aceb2f1a597c203
yutakaki <takaki.yu.dev@gmail.com>
2022-08-17 21:27:26 +0900

    add loading state

--- pages/verify-email/index.tsx
+++ pages/verify-email/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from "next";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PrivateRoute from "../../utils/PrivateRoute";
 import { defaultGetServerSidePropFunc } from "../../utils/common";
 import { useAppSelector } from "../../hooks/reduxhook";
@@ -9,8 +9,10 @@ import axios from "axios";
 const VerifyEmail = () => {
   const user = useAppSelector((state) => state.userReducer.user);
   const [resendStatus, setResendStatus] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const resendEmail = async () => {
+    setLoading(true);
     try {
       await get("/api/auth/resend-verify");
     } catch (error) {
@@ -21,7 +23,9 @@ const VerifyEmail = () => {
       }
     }
     setResendStatus(true);
+    setLoading(false);
   };
+
   return (
     <main>
       <div className="card px-5 max-w-lg w-[80%] py-10 absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/4 flex flex-col gap-8 items-center">
@@ -34,14 +38,17 @@ const VerifyEmail = () => {
           Just click on the link in that email to complete your signup. If you{" "}
           {"don't"} see it, you may need to <b>check your spam</b> folder
         </p>
-        <div className="flex flex-col gap-3 align-center">
-          {resendStatus && (
+        <div className="flex flex-col gap-3 align-center w-full items-center">
+          {resendStatus === true && (
             <p className="text-center">
               Verification email send. Please check your email.
             </p>
           )}
           <p className="text-center">Still {"can't"} find the email?</p>
-          <button className="btn" onClick={resendEmail}>
+          <button
+            className={`btn w-1/2 max-w-[200px] ${loading && "opacity-60"}`}
+            onClick={resendEmail}
+            disabled={loading}>
             Resend Email
           </button>
         </div>

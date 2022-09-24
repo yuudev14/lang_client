a75183acfa8bc0805ae032f218df77d34945f7db
yutakaki <takaki.yu.dev@gmail.com>
2022-09-24 16:25:33 +0900

    add user_id in the data being emit in random chat

--- pages/random/chat.tsx
+++ pages/random/chat.tsx
@@ -2,27 +2,32 @@ import type { GetServerSideProps, NextPage } from "next";
 import { useEffect } from "react";
 import MainLayout from "../../components/Layout/MainLayout";
 import WithNavLayout from "../../components/Layout/WithNavLayout";
+import { useAppSelector } from "../../hooks/reduxhook";
 import { defaultGetServerSidePropFunc, socket } from "../../utils/constants";
 import PrivateRoute from "../../utils/PrivateRoute";
 
 const RandomChatPage: NextPage = () => {
+  const { auth, user } = useAppSelector((state) => state.userReducer);
   useEffect(() => {
-    // socket.connect();
-    // socket.emit("join-random-chat", "english");
-    // socket.emit("find-random-chat-user", "english");
-    // socket.emit("waiting random chat match", "english");
-    // socket.on("found-random-chat-user", (id) => {
-    //   if (id) {
-    //     socket.off("found-random-chat-user");
-    //   } else {
-    //     socket.emit("waiting random chat match", "english");
-    //   }
-    // });
-    // return () => {
-    //   socket.off("found-random-chat-user");
-    //   socket.emit("disconnect-random-chat", "english");
-    //   socket.disconnect();
-    // };
+    socket.connect();
+    socket.emit("join-random-chat", "english");
+    socket.emit("find-random-chat-user", {
+      room: "english",
+      user_id: user._id,
+    });
+    socket.emit("waiting random chat match", "english");
+    socket.on("found-random-chat-user", (id) => {
+      if (id) {
+        socket.off("found-random-chat-user");
+      } else {
+        socket.emit("waiting random chat match", "english");
+      }
+    });
+    return () => {
+      socket.off("found-random-chat-user");
+      socket.emit("disconnect-random-chat", "english");
+      socket.disconnect();
+    };
   }, []);
   return (
     <MainLayout>

a6ff97d3c5a21657f0c65de6c3e7728bbdfd5132
yutakaki <takaki.yu.dev@gmail.com>
2022-11-23 20:24:54 +0900

    add sub menu for the random obtions

--- components/pages/random/chat/RandomChatOptions.tsx
+++ components/pages/random/chat/RandomChatOptions.tsx
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 import { IoMdOptions } from "react-icons/io";
+import { languages } from "../../../../utils/constants";
 
 const RandomChatOptions = () => {
   const [optionState, setOptionState] = useState(false);
   return (
     <div className="w-full flex flex-row">
-      <div className="ml-auto relative">
+      <div className="relative">
         <IoMdOptions
           size={20}
           cursor="pointer"
           onClick={() => setOptionState(!optionState)}
         />
         {optionState && (
-          <section className="absolute bg-white right-0 rounded-md shadow-lg border-2 border-solid mt-2">
+          <section className=" absolute bg-white left-0 rounded-md shadow-lg border-2 border-solid mt-2">
             <ul className="flex flex-col w-max">
-              <li className="hover:bg-slate-200 px-4 py-2 cursor-pointer">
-                Translate Message
+              <li className="hover:bg-slate-200 px-4 py-2 cursor-pointer relative">
+                Translate
+                <div className="min-w-[100px] max-w-max absolute left-full top-0 ml-3 bg-white rounded-md shadow-lg border-2 border-solid">
+                  <ul>
+                    {languages.map((lang) => (
+                      <li className="px-4 py-2" key={lang.name}>
+                        <div className="flex items-center">
+                          <span>{lang.language}</span>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </li>
             </ul>
           </section>

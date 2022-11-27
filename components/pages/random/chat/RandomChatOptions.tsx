7002e42aa0fc0bc6e61920b62b687916826314ef
yutakaki <takaki.yu.dev@gmail.com>
2022-11-27 13:17:14 +0900

    add random optin in chat page

--- components/pages/random/chat/RandomChatOptions.tsx
+++ components/pages/random/chat/RandomChatOptions.tsx
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoMdOptions } from "react-icons/io";
 import { languages } from "../../../../utils/constants";
+import random_chat_style from "../../../../styles/random/chat/random_option.module.scss";
 
 const RandomChatOptions = () => {
   const [optionState, setOptionState] = useState(false);
+  const [language, setLanguage] = useState("");
+
+  useEffect(() => {
+    // check if local storage exist
+    const currTranslateLang = localStorage.getItem("translate");
+    if (currTranslateLang) {
+      setLanguage(currTranslateLang);
+    }
+  }, []);
+
+  // open and close sub option
+  const open_sub_option = (id: string) => {
+    document.querySelector(id)?.classList.toggle("hidden");
+  };
+
+  const setLangHandler = (lang: string) => {
+    localStorage.setItem("translate", lang);
+    setLanguage(lang);
+    setOptionState(false);
+  };
+
   return (
     <div className="w-full flex flex-row">
       <div className="relative">
@@ -13,14 +35,21 @@ const RandomChatOptions = () => {
           onClick={() => setOptionState(!optionState)}
         />
         {optionState && (
-          <section className=" absolute bg-white left-0 rounded-md shadow-lg border-2 border-solid mt-2">
+          <section className="absolute bg-white left-0 rounded-md shadow-lg border-2 border-solid mt-2">
             <ul className="flex flex-col w-max">
-              <li className="hover:bg-slate-200 px-4 py-2 cursor-pointer relative">
-                Translate
-                <div className="min-w-[100px] max-w-max absolute left-full top-0 ml-3 bg-white rounded-md shadow-lg border-2 border-solid">
+              <li
+                className={random_chat_style.random_options}
+                onClick={() => open_sub_option("#translate_sub_menu")}>
+                {`Translate (${language ? language : "None"})`}
+                <div
+                  className={`${random_chat_style.sub_menu} hidden`}
+                  id="translate_sub_menu">
                   <ul>
                     {languages.map((lang) => (
-                      <li className="px-4 py-2" key={lang.name}>
+                      <li
+                        className={random_chat_style.random_options}
+                        onClick={() => setLangHandler(lang.language)}
+                        key={lang.name}>
                         <div className="flex items-center">
                           <span>{lang.language}</span>
                         </div>

ca0338cc21e4660f2bbe3d267d3a591a7de35c5d
yutakaki <takaki.yu.dev@gmail.com>
2023-01-16 23:11:54 +0900

    add layout for the meetup page

--- pages/meetup.tsx
+++ pages/meetup.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import type { GetServerSideProps, NextPage } from "next";
 import MainLayout from "../components/Layout/MainLayout";
 import WithNavLayout from "../components/Layout/WithNavLayout";
@@ -8,7 +9,29 @@ const MeetupPage: NextPage = () => {
   return (
     <MainLayout>
       <WithNavLayout>
-        <h1>hi</h1>
+        <div className="flex gap-2">
+          <div>
+            <h2>Meet and learn with someone!</h2>
+            <form>
+              <div>
+                <input type="text" placeholder="Search for keywords" />
+                <input type="text" placeholder="Neighborhood or city" />
+              </div>
+              <button className="btn">Search</button>
+            </form>
+          </div>
+          <div>
+            <h2>See what's happening</h2>
+            <ul className="flex flex-nowrap gap-1">
+              <li className="">Starting soon</li>
+              <li>Today</li>
+              <li>Tomorrow</li>
+              <li>This week</li>
+              <li>Online</li>
+              <li>In Person</li>
+            </ul>
+          </div>
+        </div>
       </WithNavLayout>
     </MainLayout>
   );
